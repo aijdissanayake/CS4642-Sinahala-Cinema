c70a0dfdcadecf2dbc05b743468c91c561b3d076
Achala Dissanayake <aidissanayake@gmail.com>
2018-07-28 13:45:56 +0530

    processed film  JSON

--- sinhala_cinema/spiders/films_spider.py
+++ sinhala_cinema/spiders/films_spider.py
@@ -18,6 +18,7 @@ class FilmsSpider(scrapy.Spider):
 
     def parse(self, response):
         films = response.css("table").extract()
+        films_set = set()
         for film in films:
             film = Selector(text=film)
             film_type = None
@@ -25,14 +26,18 @@ class FilmsSpider(scrapy.Spider):
                 film_type = film.xpath('//span[text()[contains(.,"")]]/following-sibling::text()').extract()[1]
             except :
                 pass
-            yield {
-                'title': film.css('td.FilmTitle a::text').extract_first(),
-                'english_title': film.css('span.sinhala18::text').extract_first(),
-                'main_actor' : film.xpath('//span[text()[contains(.,"Actor")]]/following-sibling::a/text()').extract_first(),
-                'main_actress' : film.xpath('//span[text()[contains(.,"Actress")]]/following-sibling::a/text()').extract_first(),
-                'producer' : film.xpath('//span[text()[contains(.,"Producer")]]/following-sibling::a/text()').extract_first(),
-                'director' : film.xpath('//span[text()[contains(.,"Director")]]/following-sibling::a/text()').extract_first(),
-                'released_date' : film.xpath('//span[text()[contains(.,"Released Date")]]/following-sibling::text()').extract_first(),
-                'category' : film.xpath('//span[text()[contains(.,"Category")]]/following-sibling::text()').extract_first(),
-                'type' : film_type
-            }
+            title = film.css('td.FilmTitle a::text').extract_first()
+            if(title):
+                if title not in films_set:
+                    films_set.add(title)
+                    yield {
+                        'title': title,
+                        'english_title': film.css('span.sinhala18::text').extract_first(),
+                        'main_actor' : film.xpath('//span[text()[contains(.,"Actor")]]/following-sibling::a/text()').extract_first(),
+                        'main_actress' : film.xpath('//span[text()[contains(.,"Actress")]]/following-sibling::a/text()').extract_first(),
+                        'producer' : film.xpath('//span[text()[contains(.,"Producer")]]/following-sibling::a/text()').extract_first(),
+                        'director' : film.xpath('//span[text()[contains(.,"Director")]]/following-sibling::a/text()').extract_first(),
+                        'released_date' : film.xpath('//span[text()[contains(.,"Released Date")]]/following-sibling::text()').extract_first(),
+                        'category' : film.xpath('//span[text()[contains(.,"Category")]]/following-sibling::text()').extract_first(),
+                        'type' : film_type
+                    }
